b09e7474dd659b80f8b5049b23d4de04eaf85fb6
Kelvin Fonseca <kelvin.f.fonseca@gmail.com>
2025-05-30 23:08:24 +0100

    feat: type hinting for db models and cleanup

---
+++ app/models/likes.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from ..context import db
+from sqlalchemy import DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Likes(db.Model):
+    __tablename__ = "likes"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    video_id: Mapped[int] = mapped_column(ForeignKey("videos.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=datetime.now
+    )
